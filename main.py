dc09b253116ae6d3889fd4c33314199fdfe0c42f
Sudip modi <sudipinshunya@gmail.com>
2023-12-31 21:38:32 +0530

    aspect api

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from transformers import pipeline
 import pandas as pd
 from flask_cors import CORS
 import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 CORS(app)
@@ -12,7 +13,6 @@ sentiment_analysis = pipeline(model="finiteautomata/bertweet-base-sentiment-anal
 def category_analyze_feedback():
     try:
         file = request.files['file']
-        aspect = request.form.get('selectedAspect')  
         df = pd.read_csv(file)
         results = []
         for index, row in df.iterrows():
@@ -32,10 +32,9 @@ def category_analyze_feedback():
     
 
 @app.route('/analyze_datewise', methods=['POST'])
-def datewise_analyze_sentiments():
+def analyze_datewise():
     try:
         file = request.files['file']
-        aspect = request.form.get('selectedAspect') 
         df = pd.read_csv(file)
         results = []
         sentimentLabelColumn=[]
@@ -66,5 +65,50 @@ def datewise_analyze_sentiments():
         print(e)
         return jsonify({'error': str(e)}), 500
 
+@app.route('/analyze_aspect', methods=['POST'])
+def analyze_aspect():
+    try:
+        # Read the CSV file from the request
+        file = request.files['file']
+        df = pd.read_csv(file)
+        
+        # Aspects to analyze
+        aspects = ['Quality', 'Content', 'Instructor', 'Material', 'Engagement', 'Application', 'Structure']
+
+        # Initialize sentiment analyzer
+        sid = SentimentIntensityAnalyzer()
+
+        # Analyze sentiment for each aspect across all reviews
+        aspect_sentiments = {aspect: {'positive': 0, 'neutral': 0, 'negative': 0} for aspect in aspects}
+
+        # Analyze sentiment for each review and aspect
+        for review in df['review']:
+            for aspect in aspects:
+                if aspect.lower() in review.lower():
+                    aspect_text = review.lower().split(aspect.lower())[1].split('.')[0]
+                    analysis = sid.polarity_scores(aspect_text)['compound']
+
+                    if analysis >= 0.05:
+                        aspect_sentiments[aspect]['positive'] += 1
+                    elif -0.05 < analysis < 0.05:
+                        aspect_sentiments[aspect]['neutral'] += 1
+                    else:
+                        aspect_sentiments[aspect]['negative'] += 1
+
+        # Prepare data for response
+        response_data = {
+            'aspects': aspects,
+            'positive_counts': [aspect_sentiments[aspect]['positive'] for aspect in aspects],
+            'neutral_counts': [aspect_sentiments[aspect]['neutral'] for aspect in aspects],
+            'negative_counts': [aspect_sentiments[aspect]['negative'] for aspect in aspects]
+        }
+
+        print(jsonify(response_data))
+        return jsonify(response_data)
+
+    except Exception as e:
+        print(jsonify({'error': str(e)}))
+        return jsonify({'error': str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
